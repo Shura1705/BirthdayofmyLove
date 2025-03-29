8248ab1bad03efa5211de0232138a5da05b650ff
Shura1705 <ksahil14960@gmail.com>
2025-03-29 10:16:39 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,5 @@
+// Function to navigate between pages
+function navigateTo(page) {
+    const repoName = "birthday-surprise"; // Replace with your GitHub repository name
+    window.location.href = `/${repoName}/${page}`;
+}
